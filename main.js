622980f126053423f4408df59a818382969abc8e
Mohamed Sabry <99232657+moSabryii@users.noreply.github.com>
2024-10-27 00:42:49 +0300

    initial commit for studying notes of JS

---
+++ main.js
@@ -0,0 +1,15 @@
+//comments
+// is used to make a one line comment
+/* is used to make multiple line comments */
+// you can comment a section using ctrl + /
+
+/****************************************************************/
+// note that what document is meant by is the html file
+// prints a message in the console
+console.log("Hello from JS file");
+
+//sends an alert message to the window screen
+window.alert("This is my first alert");
+
+// writes changes to the html file eg. adding heading 1 element
+document.write("<h1>Hello world</h1>");
